3571be29eb0358e9b5304bf06bf99dc41640e69c
GermanRusco <121166245+GermanRusco@users.noreply.github.com>
2023-02-28 02:02:36 -0300

    Se corrige código

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,4 +1,4 @@
 //Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
 
-var nombre = prompt("Escribe tu nombre: ");
+let nombre = prompt("Escribe tu nombre: ");
 document.write("Hola "+nombre); 
